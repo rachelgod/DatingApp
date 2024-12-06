90253e3dc186949f777dd8dd82ba266b3b83d0e2
rachel.godfrey <rachel.godfrey@icumed.com>
2024-12-05 16:26:35 -0800

    Added new components and routing

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,26 +2,30 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 //import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
   // private accountService = inject(AccountService);
-  accountService = inject(AccountService);
   // loggedIn = false;
+  accountService = inject(AccountService);
+  private router = inject(Router);
+
   model: any = {};
 
   login() {
     console.log(this.model);
     this.accountService.login(this.model).subscribe({
-      next: (response) => {
-        console.log(response);
+      next: () => {
+        this.router.navigateByUrl('/members');
+        // console.log(response);
         // this.loggedIn = true;
       },
       error: (error) => console.log(error),
@@ -31,5 +35,6 @@ export class NavComponent {
   logout() {
     // this.loggedIn = false;
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
