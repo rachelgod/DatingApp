b20a2c1e4470f5055d080d849d5a5790cda27d54
rachel.godfrey <rachel.godfrey@icumed.com>
2024-12-17 17:08:42 -0800

    End of section 7 - error handling

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -9,27 +9,44 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class BuggyController(DataContext context): BaseApiController
+    public class BuggyController(DataContext context) : BaseApiController
     {
         [Authorize]
         [HttpGet("auth")]
         public ActionResult<string> GetAuth()
-        { 
+        {
             return "secret text";
         }
 
         [HttpGet("not-found")]
         public ActionResult<AppUser> GetNotFound()
-        { 
+        {
             var thing = context.Users.Find(-1);
             if (thing == null) return NotFound();
 
             return thing;
         }
 
+        // [HttpGet("server-error")]
+        // public ActionResult<AppUser> GetServerError()
+        // {
+        //     try
+        //     {
+        //         var thing = context.Users.Find(-1) ?? throw new Exception("A bad thing has happened");
+
+        //         return thing;
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         return StatusCode(500, "Computer says no!");
+        //     }
+
+        // }
+
         [HttpGet("server-error")]
         public ActionResult<AppUser> GetServerError()
-        { 
+        {
+
             var thing = context.Users.Find(-1) ?? throw new Exception("A bad thing has happened");
 
             return thing;
@@ -37,7 +54,7 @@ namespace API.Controllers
 
         [HttpGet("bad-request")]
         public ActionResult<string> GetBadRequest()
-        { 
+        {
 
             return BadRequest("This was not a good request");
         }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +14,6 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
-// // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
 // {
 //     app.UseSwagger();
@@ -24,6 +24,8 @@ var app = builder.Build();
 
 // app.UseAuthorization();
 
+// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseAuthentication();
