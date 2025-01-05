aece7ef1c5e6111cb601b150b5b29096b68fcade
Rachel <rkgodfrey@gmail.com>
2025-01-04 22:33:15 -0800

    End of section 10 added spinner and steamlined loading process to keep member in signal instead of loading every time

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -8,11 +8,14 @@ import { provideToastr } from 'ngx-toastr';
 import { errorInterceptor } from './_interceptors/error.interceptor';
 import { jwtInterceptor } from './_interceptors/jwt.interceptor';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { loadingInterceptor } from './_interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor])),
+    provideHttpClient(
+      withInterceptors([errorInterceptor, jwtInterceptor, loadingInterceptor])
+    ),
     provideAnimations(),
     provideToastr({
       positionClass: 'toast-bottom-right', //'toast-center-center',
